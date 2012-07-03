ee2a8a44a670f7ace11d3c63f7428433be4a2223
Kevin Triplett <kevin@mopacmedia.com>
2012-07-02 22:28:11 -0500

    fix populate file

--- db/populate/development/02_type_of_assets.rb
+++ db/populate/development/02_type_of_assets.rb
@@ -1,6 +1,6 @@
 TypeOfAsset.create_or_update(:id => 1, :name => "Cash")
-TypeOfAsset.create_or_update(:id => 1, :name => "Check")
-TypeOfAsset.create_or_update(:id => 1, :name => "Credit")
-TypeOfAsset.create_or_update(:id => 1, :name => "Coupon")
-TypeOfAsset.create_or_update(:id => 1, :name => "Share")
+TypeOfAsset.create_or_update(:id => 2, :name => "Check")
+TypeOfAsset.create_or_update(:id => 3, :name => "Credit")
+TypeOfAsset.create_or_update(:id => 4, :name => "Coupon")
+TypeOfAsset.create_or_update(:id => 5, :name => "Share")
 
